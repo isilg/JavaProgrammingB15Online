891b87d8bb65ae956c98d8abaeb963d09b98a2ce
isilg <isilavunduk@gmail.com>
2020-01-16 15:14:46 +0300

    Day02

---
+++ src/day02/AboutMe.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class AboutMe {    //Class name is AboutMe here. Don't change class name!!! It has to be same name with the file name under the package(day02 is our package. Under day02 we have AboutMe file. If we change the name from program that 2 names are not gonna be same and it will be problem)
+
+    public static void main(String[] arg){
+
+        System.out.println("My name is Isil");
+
+
+
+    }
+}
