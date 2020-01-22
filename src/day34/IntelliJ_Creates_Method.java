429624cd4bca75f1534aa63a3b6be6160db8727a
isilg <isilavunduk@gmail.com>
2020-01-22 15:13:00 +0300

    Math Action

---
+++ src/day34/IntelliJ_Creates_Method.java
@@ -0,0 +1,42 @@
+package day34;
+public class IntelliJ_Creates_Method {
+    public static void main(String[] args) {
+
+        // ******* I have a login1 method but if I declare it like that
+        // It gives error. Hover over the method name, login1, it shows
+        // create method login1. Click it. IntelliJ creates is automatically *****
+        loginVoid("my username" , "abc123");
+        loginVoid("user", "abc123");
+
+        boolean result = loginReturn("user","abc123");
+        System.out.println("result = "+ result);
+
+        System.out.println("second run "+ loginReturn("aaa", "bbb"));
+
+        // System.out.println( loginVoid("aaaa", "bbb") );  //CAN NOT DO IT WITH VOID METHOD
+        // boolean result2 = loginVoid("abc", "efg");       //NO RESULT TO SAVE!
+
+        // I CAN CALL MY METHOD INSIDE IF
+        if ( loginReturn("user1", "abc123") ) {
+            System.out.println("Add Java Book to cart");
+            System.out.println("Pay for Java Book in cart");
+            System.out.println("View The order ");
+
+        } else {
+            System.out.println("NO SHOPPING UNLESS YOU SIGNED IN!!!!!!");
+        }
+
+
+    }
+
+        private static void loginVoid(String user, String password) {
+           if (user.equals("user") && password.equals("abc123")){
+               System.out.println("Login Successful");
+           }else
+               System.out.println("Login failed");
+        }
+
+         private static boolean loginReturn(String user, String password) {
+            return user.equals("user") && password.equals("abc123");
+        }
+    }
--- src/day34/MathActionFromJDK.java
+++ src/day34/MathActionFromJDK.java
@@ -1,4 +1,23 @@
 package day34;
-
 public class MathActionFromJDK {
+    public static void main(String[] args) {
+
+        // When I create a String variable where is String class coming from ?
+        // it's coming from a package called java.lang
+        // it's a special package
+        // and any class in it ,does not need importing when being used
+        // for example String class
+        String str = "isil";
+
+        // There is a class under java.lang called Math
+        // and it has lots of math utility methods that ready to be used
+        // Type Math. it will give you options to choose like
+        // Math.addExact. Then u can assign it to a variable and return it
+        //or can return it directly by using  System.out.print
+        int sum = Math.addExact(10, 20);
+        System.out.println("sum = " + sum);
+
+        System.out.println("max of 10, 20 --> " + Math.max(10, 20));
+
+    }
 }
