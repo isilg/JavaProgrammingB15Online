c2e949566ebf2ed1bfe426c684b82619b6562abd
isilg <isilavunduk@gmail.com>
2020-01-22 21:37:12 +0300

    return kywrd

--- src/day34/Using_Return.java
+++ src/day34/Using_Return.java
@@ -2,12 +2,12 @@ package day34;
 public class Using_Return {
     public static void main(String[] args) {
         min4character("Jon");
-       // min4character("John");
+        //min4character("John");
     }
     public static void min4character(String name){
         if(name.length() < 4){
             System.out.println("invalid name");
-            return;   //return kywrd is used to get out of teh METHOD(try w it and without it)
+            //return;   //return kywrd is used to get out of the METHOD(try w it and without it)
         }
 
         System.out.println(name);
