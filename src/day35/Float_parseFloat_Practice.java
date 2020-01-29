3848d806d9e804ef772db110d71d346649be78f2
isilg <isilavunduk@gmail.com>
2020-01-29 15:05:53 +0300

    MethodReturnIntArray

---
+++ src/day35/Float_parseFloat_Practice.java
@@ -0,0 +1,13 @@
+package day35;
+public class Float_parseFloat_Practice {
+    public static void main(String[] args) {
+        /**
+         * How can we turn String value into float value
+         * "3.14" -->> 3.14f
+         */
+
+        float f6 = Float.parseFloat("3.14f");
+        System.out.println(f6);
+
+    }
+}
