ca5e0a9d8cdb05b47052e4ff63ab19d6d140adea
isilg <isilavunduk@gmail.com>
2020-03-11 19:04:19 -0400

    day45

--- src/day45/Dog.java
+++ src/day45/Dog.java
@@ -4,11 +4,10 @@ public class Dog extends Animal{
     public void speak(){
         //!!!!!******INTERVIEW: WHAT IS OVERRIDING AND OVERLOADING******!!!!!
 
-        //Overriding parent method. Overriding: child class has the
-        //same method(same exact method name, same exact method parameter,
-        //same exact return type, most of the time same access modifier)
-        // with the parent but child class can give its
-        //own implementation to a method
+        //Overriding parent method. Overriding: child class has the same
+        //method(same exact method name, same exact method parameter, same
+        //exact return type, most of the time same access modifier) with the
+        //parent but child class can give its own implementation to that inherited method
         System.out.println("Bark!");
     }
 
