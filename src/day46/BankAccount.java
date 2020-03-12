435caf6e74fd1b1f4d6434139dc01fff6212361a
isilg <isilavunduk@gmail.com>
2020-03-11 22:43:43 -0400

    day45

---
+++ src/day46/BankAccount.java
@@ -0,0 +1,35 @@
+package day46;
+
+public class BankAccount {
+
+    String accountHolder;
+    long accountNum;
+    double balance;
+
+
+    //always add no arg constructor in super class to
+    //avoid error in sub class because of super() call
+    public BankAccount(){
+
+    }
+
+
+   // Generate - >Constructor
+    public BankAccount(String accountHolder, long accountNum, double balance) {
+
+        //super()  even we don't see super() is the first constructor we get no matter what
+        this.accountHolder = accountHolder;
+        this.accountNum = accountNum;
+        this.balance = balance;
+    }
+
+
+    public void deposit(int amount){  //it could be double but for the simplicity we chose it as int
+        balance += amount;      //balance = this.balance+  amount
+    }
+
+    public void withdraw(int amount){
+        balance -= amount;
+    }
+
+}
---
+++ src/day46/SavingAccount.java
@@ -0,0 +1,44 @@
+package day46;
+
+public class SavingAccount extends BankAccount{
+
+    double interestRate;
+
+
+    public  SavingAccount(String accountHolder,long accountNum, double balance, double interestRate ){
+        super(accountHolder,accountNum, balance);
+        this.interestRate = interestRate;
+    }
+
+
+    public static void main(String[] args) {
+        SavingAccount s1 = new SavingAccount("Kate", 12345,50000, 4.2);
+        System.out.println(s1.toString());  //toString method olmadan hash code doner
+
+        s1.withdraw(1000);
+        System.out.println(s1);
+
+    }
+
+    //in our SavingAccount, if you withdraw money, you'll get $30 penalty
+    //so we'll override the withdraw method to reflect this requirement
+    @Override    //it is recommended typing this top of the inherited method
+    public void withdraw(int amount){
+        balance = balance - amount - 30;
+    }
+
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "SavingAccount{" +
+                "interestRate=" + interestRate +
+                ", accountHolder='" + accountHolder + '\'' +
+                ", accountNum=" + accountNum +
+                ", balance=" + balance +
+                '}';
+    }
+}
