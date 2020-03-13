aa9861fe8c2bb00a17ea8ef223c9ac06da78ecbb
isilg <isilavunduk@gmail.com>
2020-03-13 00:18:30 -0400

    day47

--- src/day47/FinalVariablesPractice.java
+++ src/day47/FinalVariablesPractice.java
@@ -1,5 +1,4 @@
 package day47;
-
 public class FinalVariablesPractice {
     public static void main(String[] args) {
 
@@ -9,5 +8,20 @@ public class FinalVariablesPractice {
         final int a = 20;
 
         // a = 20;  ERROR. re-assigning new value to the final variable
+
+      //Student s1 not FinalVariablesPractice s1. So I can reach the attributes of Student unless they are static
+        final Student s1 = new Student(101);
+        System.out.println(s1.StudentID);
+
+        //s1 = new Student(102); ERROR. Cannot assign a value to final variable
+        s1.StudentID = 200; //you can still change interval data attached to
+                            //the object. As long as it's the same object s1 pointing to
+                            //As long as the address doesn't change
+        //S1 is address and it isn't changing just the object it is pointing
+        //changing by this way( s1. ) so it is ok no error
+
+
+       // System.out.println(s1);  -> prints hash code(hashcode is address) because s1 is an address!
+        System.out.println(s1.StudentID);
     }
 }
--- src/day47/Student.java
+++ src/day47/Student.java
@@ -1,12 +1,9 @@
 package day47;
-
-import java.security.PublicKey;
-
 public class Student {
 
     int StudentID;
 
     public Student(int studentID){
-        this.StudentID = StudentID;
+        this.StudentID = studentID;
     }
 }
