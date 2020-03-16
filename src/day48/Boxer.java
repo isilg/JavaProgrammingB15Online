eaea7a27ea601a2887701bc09cac6f6f554a3a34
isilg <isilavunduk@gmail.com>
2020-03-16 15:51:07 -0400

    day48

--- src/day48/Boxer.java
+++ src/day48/Boxer.java
@@ -1,22 +1,10 @@
 package day48;
 
-interface BoxerWithBellyPouch{
-
-    public abstract void kickBox();
-
-    public abstract void carryChildInThePocket();
-
+public interface Boxer {
     public default void read(){
         System.out.println("ONLY DEFAULT METHOD CAN HAVE BODY IN INTERFACE");
     }
     public static void study(){
         System.out.println("what r u studying?");
     }
-
-
-    //If I BoxerWithBellyPouch extends Bounceable and
-    // inherit void bounce method still I cannot write body
-// @Override
-// void bounce();
-
 }
---
+++ src/day48/BoxerWithBellyPouch.java
@@ -0,0 +1,12 @@
+package day48;
+
+public interface BoxerWithBellyPouch {
+    public abstract void kickBox();
+
+    public abstract void carryChildInThePocket();
+
+    //If I BoxerWithBellyPouch extends Bounceable and
+    //inherit void bounce method still I cannot write body
+// @Override
+// void bounce();
+}
