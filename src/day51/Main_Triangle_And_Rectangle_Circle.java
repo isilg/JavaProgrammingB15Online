62f2418a1d42560cff136ba4687cd16394a7689a
isilg <isilavunduk@gmail.com>
2020-03-18 22:06:36 -0400

    day51

--- src/day51/Main_Triangle_And_Rectangle_Circle.java
+++ src/day51/Main_Triangle_And_Rectangle_Circle.java
@@ -10,5 +10,10 @@ public class Main_Triangle_And_Rectangle_Circle {
         s1.draw();
         s2.draw();
         s3.draw();
+
+        Shape[] allShapes = {s1, s2, s3, new Triangle(), new Circle()};
+        for(Shape each : allShapes){
+            each.draw();
+        }
     }
 }
--- src/day51/Shape.java
+++ src/day51/Shape.java
@@ -2,11 +2,6 @@ package day51;
 
 public abstract class Shape {
 
-    Shape[] myShapes = {"rectangle","triangle", "triangle", "circle", "circle"};
-    for(Shape each : myShapes){
-        each.draw();
-    }
-
     //ABSTRACT class has NO BODY
     public abstract void draw();
 
